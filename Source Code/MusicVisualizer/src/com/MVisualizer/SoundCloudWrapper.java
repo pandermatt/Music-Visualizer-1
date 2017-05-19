8bfc92c40b19f7d70630845748fdce9a675843cf
Caleb B. Gardner <ca.bosticgardner@my.ccsu.edu>
2017-05-19 00:29:40 -0400

    Updated Source + Jar

--- Source Code/MusicVisualizer/src/com/MVisualizer/SoundCloudWrapper.java
+++ Source Code/MusicVisualizer/src/com/MVisualizer/SoundCloudWrapper.java
@@ -9,13 +9,10 @@ import java.io.IOException;
 public class SoundCloudWrapper {
     private SoundCloud soundCloudSource = null;
     private ApiWrapper apiWrapper = null;
-    private String clientID, clientSecret;
 
     public SoundCloudWrapper(String clientID, String clientSecret, String username, String pass) throws IOException {
-        this.clientID = clientID;
-        this.clientSecret = clientSecret;
-        soundCloudSource = new SoundCloud(this.clientID, this.clientSecret, username, pass);
-        apiWrapper = new ApiWrapper(this.clientID, this.clientSecret, null, null);
+        soundCloudSource = new SoundCloud(clientID, clientSecret, username, pass);
+        apiWrapper = new ApiWrapper(clientID, clientSecret, null, null);
         apiWrapper.login(username, pass);
     }
 
